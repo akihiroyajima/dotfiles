29f1596a9ffea088623cd9162d76b03c714bcfb6
akihiroyajima <akihiroyazima@gmail.com>
2015-07-26 18:35:47 +0900

    add .tigrc

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ ln -s ~/Documents/dotfiles/.vimrc ~/.vimrc
 ln -s ~/Documents/dotfiles/.zshrc ~/.zshrc
 ln -s ~/Documents/dotfiles/.zprofile ~/.zprofile
 ln -s ~/Documents/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/Documents/dotfiles/.tigrc ~/.tigrc
