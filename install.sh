5ae56e1510c6972ce2f3636d1b76d4afc92d53c4
Akihiro Yajima <akihiroyazima@gmail.com>
2018-03-26 21:42:37 +0900

    Add .gitignote_global into install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ ln -s ~/dotfiles/.zprofile ~/.zprofile
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.tigrc ~/.tigrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
