39aba48fb22d97cf13ad1b3dacee322bae5888a4
Akihiro Yajima <akihiroyazima@gmail.com>
2018-04-10 12:29:41 +0900

    Add .zshrc_export

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ ln -s ~/dotfiles/.vim/rc.d ~/.vim/rc.d
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
 ln -s ~/dotfiles/.zprofile ~/.zprofile
+ln -s ~/dotfiles/.zshrc_export ~/.zshrc_export
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.tigrc ~/.tigrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
